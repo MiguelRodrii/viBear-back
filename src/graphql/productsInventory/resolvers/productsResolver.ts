1c5655379f268d8602ded8321036c46474b1f676
Miguel Rodríguez <miguelrodrii@outlook.com>
2021-03-31 23:27:33 -0500

    create inputs and resolvers created and tested

--- src/graphql/productsInventory/resolvers/productsResolver.ts
+++ src/graphql/productsInventory/resolvers/productsResolver.ts
@@ -33,30 +33,101 @@ const productsResolver = {
       return result.rows;
     },
   },
-  ProductType: {
-    iva_percentage: async (productType: { iva_percentage_id: number; }) => {
-      const result = await db.query(`select ip.* from products_inventory.iva_percentages ip, products_inventory.product_types pt where ip.id = ${productType.iva_percentage_id};`);
+  Mutation: {
+    createProductType: async (
+      parent: Record<string, unknown>,
+      { productType }: {
+        productType: {
+          name: string;
+          is_expirable: boolean;
+          iva_percentage_id: number;
+        };
+      },
+    ) => {
+      const result = await db.query(
+        `insert into products_inventory.product_types ("name", is_expirable, iva_percentage_id) values ('${productType.name}', ${productType.is_expirable}, ${productType.iva_percentage_id}) returning *;
+      `,
+      );
       return result.rowCount === 0 ? [] : result.rows[0];
-    }
+    },
+    createProductDefinition: async (
+      parent: Record<string, unknown>,
+      { productDefinition }: {
+        productDefinition: {
+          name: string;
+          description: string;
+          product_type_id: number;
+        };
+      },
+    ) => {
+      const result = await db.query(
+        `insert into products_inventory.product_definitions ("name", description, product_type_id) values ('${productDefinition.name}', '${productDefinition.description}', ${productDefinition.product_type_id}) returning *;
+      `,
+      );
+      return result.rowCount === 0 ? [] : result.rows[0];
+    },
+    createProduct: async (
+      parent: Record<string, unknown>,
+      { product }: {
+        product: {
+          purchase_price: number;
+          sale_price: number;
+          initial_amount: number;
+          current_amount: number;
+          product_definition_id: number;
+        };
+      },
+    ) => {
+      const result = await db.query(
+        `insert into products_inventory.products (initial_amount, current_amount, purchase_price, sale_price, product_definition_id) values 
+      (${product.initial_amount}, ${product.current_amount}, ${product.purchase_price}, ${product.sale_price}, ${product.product_definition_id}) returning *;`,
+      );
+      return result.rowCount === 0 ? [] : result.rows[0];
+    },
+    createExpirationDate: async (
+      parent: Record<string, unknown>,
+      { expirationDate }: {
+        expirationDate: { value: Date; product_id: number };
+      },
+    ) => {
+      const result = await db.query(
+        `insert into products_inventory.expiration_dates (value, product_id) values ('${expirationDate.value}', ${expirationDate.product_id}) returning *;`,
+      );
+      return result.rowCount === 0 ? [] : result.rows[0];
+    },
+  },
+  ProductType: {
+    iva_percentage: async (productType: { iva_percentage_id: number }) => {
+      const result = await db.query(
+        `select ip.* from products_inventory.iva_percentages ip, products_inventory.product_types pt where ip.id = ${productType.iva_percentage_id};`,
+      );
+      return result.rowCount === 0 ? [] : result.rows[0];
+    },
   },
   ProductDefinition: {
-    product_type: async (productDefinition: { product_type_id: number; }) => {
-      const result = await db.query(`select pt.* from products_inventory.product_types pt, products_inventory.product_definitions pd where pt.id = ${productDefinition.product_type_id};`);
+    product_type: async (productDefinition: { product_type_id: number }) => {
+      const result = await db.query(
+        `select pt.* from products_inventory.product_types pt, products_inventory.product_definitions pd where pt.id = ${productDefinition.product_type_id};`,
+      );
       return result.rowCount === 0 ? [] : result.rows[0];
-    }
+    },
   },
   Product: {
-    product_definition: async (product: {product_definition_id: number}) => {
-      const result = await db.query(`select pd.* from products_inventory.product_definitions pd, products_inventory.products p where pd.id = ${product.product_definition_id};`);
+    product_definition: async (product: { product_definition_id: number }) => {
+      const result = await db.query(
+        `select pd.* from products_inventory.product_definitions pd, products_inventory.products p where pd.id = ${product.product_definition_id};`,
+      );
       return result.rowCount === 0 ? [] : result.rows[0];
-    }
+    },
   },
   ExpirationDate: {
-    product: async (expirationDate: {product_id: number}) => {
-      const result = await db.query(`select p.* from products_inventory.products p, products_inventory.expiration_dates ed where p.id = ${expirationDate.product_id};`);
+    product: async (expirationDate: { product_id: number }) => {
+      const result = await db.query(
+        `select p.* from products_inventory.products p, products_inventory.expiration_dates ed where p.id = ${expirationDate.product_id};`,
+      );
       return result.rowCount === 0 ? [] : result.rows[0];
-    }
-  }
+    },
+  },
 };
 
 export default productsResolver;
--- src/graphql/productsInventory/types/productsType.ts
+++ src/graphql/productsInventory/types/productsType.ts
@@ -11,6 +11,13 @@ type Query {
     expirationDates: [ExpirationDate]
 }
 
+type Mutation {
+    createProductType(productType: ProductTypeInput): ProductType
+    createProductDefinition(productDefinition: ProductDefinitionInput): ProductDefinition
+    createProduct(product: ProductInput): Product
+    createExpirationDate(expirationDate: ExpirationDateInput): ExpirationDate
+}
+
 type IvaPercentage {
     id: Int
     value: Float
@@ -22,12 +29,22 @@ type ProductType {
     is_expirable: Boolean
     iva_percentage: IvaPercentage    
 }
+input ProductTypeInput {
+    name: String!
+    is_expirable: Boolean!
+    iva_percentage_id: Int!
+}
 
 type ProductDefinition {
     id: Int
     name: String
     description: String
     product_type: ProductType    
+}
+input ProductDefinitionInput {
+    name: String!
+    description: String!
+    product_type_id: Int!
 }
 
 type Product {
@@ -38,11 +55,22 @@ type Product {
     current_amount: Int
     product_definition: ProductDefinition
 }
+input ProductInput {
+    purchase_price: Float!
+    sale_price: Float!
+    initial_amount: Int!
+    current_amount: Int!
+    product_definition_id: Int!
+}
 
 type ExpirationDate {
     id: Int
     product: Product
     value: Date
+}
+input ExpirationDateInput {
+    product_id: Int!
+    value: Date!
 }
 `);
 
